dcaa32e208bccd43cf518daab4b754b86996a654
David Verbustel <david.verbustel@gmail.com>
2015-07-20 17:28:32 +0200

    added bootstrap, styled forms and alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  BOOTSTRAP_FLASH_MSG = {
+  success: 'alert-success',
+  error: 'alert-danger',
+  alert: 'alert-danger',
+  notice: 'alert-success'
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type.to_s)
+  end
 end
