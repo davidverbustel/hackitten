2e5d1eb1c907ea79cfed428cc2991bd0648bf94c
David Verbustel <david.verbustel@gmail.com>
2015-07-22 00:19:31 +0200

    added fb connect and link in navbar

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
-ruby '2.2.2'
+gem 'simple_form', '~> 3.1.0'
+gem 'omniauth-facebook'
+gem 'figaro'
 gem 'rails_12factor', group: :production
 gem 'puma',           group: :production
 gem "letter_opener", group: :development
@@ -36,6 +38,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+ruby '2.2.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713213911) do
+ActiveRecord::Schema.define(version: 20150721215705) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20150713213911) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "picture"
+    t.string   "name"
+    t.string   "token"
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
