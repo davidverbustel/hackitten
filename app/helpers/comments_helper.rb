54b4b94e370102846ba5ef42885ef51bed399694
David Verbustel <david.verbustel@gmail.com>
2015-07-30 19:58:12 +0200

    added pluralize to comments with new helper comment

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,11 @@
 module CommentsHelper
+  def pluralize_comment(count, singular, plural = nil)
+    word = if (count == 0 || count == 1 || count =~ /^1(\.0+)?$/)
+      singular
+    else
+      plural || singular.pluralize
+    end
+
+    "#{count || 0} #{word}"
+  end
 end
