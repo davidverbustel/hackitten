901bd494e3bb074a21cfd6bd111e5908ed80f81e
David Verbustel <david.verbustel@gmail.com>
2015-07-31 16:26:24 +0200

    grab medata from URL and display them on show link view

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,14 @@
+require 'embedly'
+require 'json'
+
 module LinksHelper
+
+  def display(url)
+    embedly_api = Embedly::API.new(key: ENV['EB_KEY'])
+    obj = embedly_api.oembed :url => url, width
+    # obj = embedly_api.extract :url => url
+    # (obj.first.html).html_safe
+    (obj.first.html).html_safe
+    # raise JSON.pretty_generate(obj[0].marshal_dump)
+  end
 end
